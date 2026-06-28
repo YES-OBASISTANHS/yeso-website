e9a963396e46a75165dee81425a881bcac9df487
YES-OBASISTANHS <yesobasistanhs@gmail.com>
2026-06-28 14:17:00 +0800

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,42 +1,25 @@
-import { auth } from ".firebase.js";
+import { auth } from "./firebase.js";
 
 import {
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
 
-signInWithEmailAndPassword
+const form = document.getElementById("loginForm");
 
+if (form) {
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then(() => {
+        window.location.href = "members.html";
+      })
+      .catch((error) => {
+        document.getElementById("loginMessage").textContent =
+          error.message;
+      });
+  });
 }
-
-from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
-const form=document.getElementById("loginForm");
-
-if(form){
-
-form.addEventListener("submit",e=>{
-
-e.preventDefault();
-
-const email=document.getElementById("email").value;
-
-const password=document.getElementById("password").value;
-
-signInWithEmailAndPassword(auth,email,password)
-
-.then(()=>{
-
-window.location.href="member.html";
-
-})
-
-.catch(error=>{
-
-document.getElementById("loginMessage").textContent=
-
-error.message;
-
-});
-
-});
-
-}
-
