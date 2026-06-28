ecb5f163b2e7395c576c8202db29193b5b4cea99
YES-OBASISTANHS <yesobasistanhs@gmail.com>
2026-06-28 15:36:33 +0800

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -3,7 +3,15 @@ import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-storage.js";
 
-// ...firebaseConfig...
+const firebaseConfig = {
+  apiKey: "AIzaSyD6lq2K4qWQZWryyCaUR0IqSZgMBCNF8f8",
+  authDomain: "yes-o-website.firebaseapp.com",
+  projectId: "yes-o-website",
+  storageBucket: "yes-o-website.firebasestorage.app",
+  messagingSenderId: "797990532058",
+  appId: "1:797990532058:web:6814ba37c3e2d0cdbf06e3",
+  measurementId: "G-J8KN8VPD3J"
+};
 
 const app = initializeApp(firebaseConfig);
 
