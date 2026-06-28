15680e6d9b4493d546b5f33117cdb3fb95ada65b
YES-OBASISTANHS <yesobasistanhs@gmail.com>
2026-06-28 15:08:04 +0800

    Create admin-auth.js

---
+++ js/admin-auth.js
@@ -0,0 +1,40 @@
+import { auth, db } from "js/firebase.js";
+
+import {
+    onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
+
+import {
+    doc,
+    getDoc
+} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
+
+onAuthStateChanged(auth, async(user)=>{
+
+    if(!user){
+
+        location.href="login.html";
+
+        return;
+
+    }
+
+    const snap=await getDoc(doc(db,"users",user.uid));
+
+    if(!snap.exists()){
+
+        location.href="login.html";
+
+        return;
+
+    }
+
+    if(snap.data().role!=="admin"){
+
+        alert("Administrator access only.");
+
+        location.href="members.html";
+
+    }
+
+});
